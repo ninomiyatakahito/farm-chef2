783daabad85d4f22844fb434aadfc9720eacc606
ninomiyatakahito <ghexcffb@gmail.com>
2021-02-28 21:10:55 +0900

    インスタンス定義

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,9 @@
 class UsersController < ApplicationController
   def show
+    @user = User.find(params[:id])
+    @name = user.name
+    @prefecture = user.prefecture
+    @business_name = user.business_name
+    @profile = user.profile
   end
 end
