00b0fe07c1552ecb3eb6073beff1c10f401f296e
ninomiyatakahito <ghexcffb@gmail.com>
2021-02-28 21:45:09 +0900

    ルーティング記述

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,14 @@ Rails.application.routes.draw do
   get 'comments/create'
   devise_for :users
   get 'posts/index'
-  root to:"posts#index"
+  root to:"posts#home"
 
   resources :posts do
     resources :comments
+    
+    member do
+      get 'home'
+    end
   end
 
   resources :users
