f8509d7aa60eae257a1a121c93e716d1ba8f512d
ninomiyatakahito <ghexcffb@gmail.com>
2021-02-28 20:29:45 +0900

    コメントのインスタンス変数定義

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,10 @@
 class CommentsController < ApplicationController
   def create
+    @comment = Comment.new(comment_params)
+    if @comment.save
+      redirect_to post_path(@comment.post)
+      else
+      render "posts/show"
+    end
   end
 end
