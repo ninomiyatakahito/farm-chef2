83342323006ee7c2efe61477f1af0ffdfade68cb
ninomiyatakahito <ghexcffb@gmail.com>
2021-02-28 19:50:31 +0900

    編集のアクション定義

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -17,6 +17,19 @@ class PostsController < ApplicationController
       render :new
     end
   end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+  
   
   private
 
