0135e1711e52362283e15f594066e21889aa8526
ninomiya <71581462+ninomiyatakahito@users.noreply.github.com>
2021-02-28 18:38:27 +0900

    Revert "ユーザーの投稿機能を実装"

--- db/migrate/20210228092806_create_posts.rb
+++
@@ -1,9 +0,0 @@
-class CreatePosts < ActiveRecord::Migration[6.0]
-  def change
-    create_table :posts do |t|
-      t.string :post_text,        null: false
-      t.references :user,         foreign_key: true 
-      t.timestamps
-    end
-  end
-end
