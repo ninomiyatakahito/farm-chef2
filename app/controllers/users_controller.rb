c778c3de72afed7e05f4980d22abff91c47ab78f
ninomiyatakahito <ghexcffb@gmail.com>
2021-03-01 02:33:05 +0900

    アクションを定義

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,14 @@
 class UsersController < ApplicationController
+
+  def index
+  end
+
+  def new
+  end
+  
   def show
     @user = User.find(params[:id])
-    
+
     user = User.find(params[:id])
     @name = user.name
     @prefecture = user.prefecture
