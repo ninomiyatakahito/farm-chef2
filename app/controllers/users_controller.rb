7afc4e3bbee8633474552668b6556bdc52e244b9
ninomiyatakahito <ghexcffb@gmail.com>
2021-02-28 21:12:13 +0900

    インスタンス編集

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
+    
+    user = User.find(params[:id])
     @name = user.name
     @prefecture = user.prefecture
     @business_name = user.business_name
