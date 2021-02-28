34c9cb79ffd66e15da5cf64b67c57664f0a26b0f
ninomiyatakahito <ghexcffb@gmail.com>
2021-02-28 19:09:49 +0900

    アクション定義

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,20 @@
 class PostsController < ApplicationController
   def index
+    @posts =Post.all
   end
 
   def new
+    @post = Post.new
   end
 
   def create
+    @post = Post.new(post_params)
+    @post.user_id = current_user.id
+    if @post.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
-  
   
 end
